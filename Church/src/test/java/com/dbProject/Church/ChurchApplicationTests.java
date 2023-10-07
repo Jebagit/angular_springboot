a2ea9e9cd5f83e448e62a664f675087b3ce0b3e7
Jeba <jebavincilo@gmail.com>
2023-10-07 07:38:58 +0530

    Church Application Project added

---
+++ Church/src/test/java/com/dbProject/Church/ChurchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dbProject.Church;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChurchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
